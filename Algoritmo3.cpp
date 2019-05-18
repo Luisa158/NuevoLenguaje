46f47417897ab79ebc26593880a850fbac528f66
Luisa158 <luisa.monsalve.2019@upb.edu.co>
2019-05-18 17:39:47 -0500

    Lenguaje C++

---
+++ Algoritmo3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int num,resultado;
+    cout <<"Enter a number: "; cin>>num;
+    resultado = pow(num, 2);
+    cout <<"\nThe number squared is: "<<resultado<<endl;
+
+    return 0;
+}
