786d0e3eb1fd52725e14b879d74d4d00a50eaf24
Luisa158 <47455131+Luisa158@users.noreply.github.com>
2019-05-18 20:00:15 -0500

    Add files via upload

---
+++ Algoritmo22.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float a;
+    cout<<"ingrese el numero, este programa pasa de segundos a miunutos"<<std::endl;
+    cin>>a;
+    float resultado=(a/60);
+    cout<<"el resultado de la conversion es:"<<resultado<<std::endl;
+    return 0;
+}
